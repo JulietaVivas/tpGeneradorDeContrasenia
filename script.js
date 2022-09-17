8dfcd478c82b1df034527952723fbd6465a68e99
Julieta Vivas <julietavivas.vj@gmail.com>
2022-09-17 17:46:34 -0300

    comienzo a trabajar en js

---
+++ script.js
@@ -0,0 +1,28 @@
+//Llamando a mis id para poder manipularlos
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+
+const $caracteres12 = $("#caracteres12");
+const $caracteres9 = $("#caracteres9");
+const $caracteres6 = $("#caracteres6");
+
+const $soloLetras = $("#soloLetras");
+const $soloNumeros = $("#soloNumeros");
+const $todosLosCaracteres = $("#todosLosCaracteres");
+
+const $mayusculas = $("#mayusculas");
+const $minusculas = $("#minusculas");
+const $numeros = $("#numeros");
+const $simbolos = $("#simbolos");
+
+const $btn = $("#btn");
+
+// Variables a trabajar 
+const mayusculas = "ABCDEFGHIJKMNLOPQRSTUVWXYZ";
+const minusculas = "abcdefghijkmnlopqrstuvwxyz";
+const numeros = "1234567890";
+const simbolos = " !  # $ % & ' ( ) * + , - . / : ; < = > ? @ [ ] ^ _` { | } ~"
+
+const select = []; 
+
+
